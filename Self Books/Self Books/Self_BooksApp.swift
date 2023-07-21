b1093f1187dc4c67bd67b587723e9d59f32a5ba4
Luca Tagliabue <lu.tagliabue@reply.it>
2023-07-21 12:09:53 +0200

    Add first project files

---
+++ Self Books/Self Books/Self_BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Self_BooksApp.swift
+//  Self Books
+//
+//  Created by softwave on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Self_BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
