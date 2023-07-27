6645622208836a4d1cc5d7992479154976344eb5
Luca Tagliabue <lu.tagliabue@reply.it>
2023-07-27 20:30:00 +0200

    Add detail view  and book image

--- Self Books/Self Books/ListView.swift
+++ Self Books/Self Books/ListView.swift
@@ -20,25 +20,39 @@ struct Book: Identifiable {
     let title: String
     let author: String
     let genre: Genre
+    let url: URL
 }
 
 func makeBooks() -> [Book] {
     
-    let infos = ["Allen Ginsberg" : ("Howl", Genre.poetry), "Gregory Corso" : ("Bomb", Genre.poetry), "Dario Bertini" : ("Prove di nuoto nella birra scura", Genre.poetry) , "Dino Buzzati" : ("La nera", Genre.noir)]
+    let howl = "https://m.media-amazon.com/images/I/41rGq6gqNdL._SX198_BO1,204,203,200_QL40_ML2_.jpg"
+    let bomb = "https://i0.wp.com/auralcrave.com/wp-content/uploads/2018/01/C9YfIRPW0AA4z4A.jpeg?w=700&ssl=1"
+    let prove = "https://www.ibs.it/images/9788890543494_0_536_0_75.jpg"
+    let nera = "https://www.oscarmondadori.it/content/uploads/2020/11/978880472570HIG.JPG"
     
-    return (0...12).map { _ -> Book in
+    let infos = ["Allen Ginsberg" : ("Howl", howl, Genre.poetry),
+                 "Gregory Corso" : ("Bomb", bomb, Genre.poetry),
+                 "Dario Bertini" : ("Prove di nuoto nella birra scura", prove, Genre.poetry) ,
+                 "Dino Buzzati" : ("La nera", nera, Genre.noir)]
+    
+    return (0...12).compactMap { _ -> Book? in
         
         let isbn = (0...13).reduce("") { acc, next in
             acc + "\(Int.random(in: 0..<10))"
         }
         
-        let (author, title, genre) = infos.enumerated().map{($1.key, $1.value.0, $1.value.1)}[Int.random(in: 0..<infos.count)]
+        let (author, title, image, genre) = infos.enumerated().map{($1.key, $1.value.0, $1.value.1, $1.value.2)}[Int.random(in: 0..<infos.count)]
+        
+        guard let url = URL(string: image) else {
+            return nil
+        }
         
         return Book(
             isbn: isbn,
             title: title,
             author: author,
-            genre: genre
+            genre: genre,
+            url: url
         )
     }
 }
@@ -58,7 +72,10 @@ struct ListView: View {
                         Text("Author: " + book.author)
                         Text("Title: " + book.title)
                         Text("Genre: " + book.genre.rawValue)
-                        Text("ISBN: " + book.isbn)
+                        
+                        NavigationLink(destination: DetailView(book: book)) {
+                                            Text("Detail")
+                                        }
                     }
                 } //see https://sarunw.com/posts/swiftui-list-section-header-footer/ for kind of list
                 
@@ -71,7 +88,7 @@ struct ListView: View {
         #endif
         .task {
             do {
-                try await Task.sleep(nanoseconds: 5_500_000_000)
+                try await Task.sleep(nanoseconds: 3_500_000_000)
                 isProgressViewHidden = true
                 books = makeBooks()
             } catch {
@@ -86,6 +103,21 @@ struct ListView_Previews: PreviewProvider {
         ForEach(Device.allCases, id: \.self) {
             Tab()
                 .previewDevice($0)
+        }
+    }
+}
+
+struct DetailView: View {
+    
+    let book: Book
+    
+    var body: some View {
+        VStack {
+            Text("Author: " + book.author)
+            Text("Title: " + book.title)
+            Text("Genre: " + book.genre.rawValue)
+            Text("ISBN: " + book.isbn)
+            AsyncImage(url: book.url) { image in image.resizable() } placeholder: { Color.red } .frame(width: 128, height: 128) .clipShape(RoundedRectangle(cornerRadius: 25))
         }
     }
 }
